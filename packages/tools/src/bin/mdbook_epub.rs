fd65c1c4f2b5fd309da7ae10f603a11c654770a4
max@inteist <max@inteist.com>
2026-03-08 14:14:50 -0400

    updated hX headers to Rustascean red

--- packages/tools/src/bin/mdbook_epub.rs
+++ packages/tools/src/bin/mdbook_epub.rs
@@ -808,6 +808,15 @@ article.chapter {{
     column-width: auto !important;
 }}
 
+h1, h2, h3, h4, h5, h6 {{
+    color: rgb(246, 67, 0);
+}}
+
+h1 a, h2 a, h3 a, h4 a, h5 a, h6 a {{
+    color: inherit;
+    text-decoration: none;
+}}
+
 pre,
 code {{
   font-family: ui-monospace, Menlo, Monaco, Consolas, monospace;
